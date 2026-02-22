46446649ddc37ae783f8bb8c612164f03a29eb70
Harald Sitter <sitter@kde.org>
2026-02-22 19:35:09 +0100

    main: port to kaboutdata
    nicer api and more idiomatic kde code

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 
 #include <libvirt/libvirt.h>
 
+#include <KAboutData>
 #include <KIconTheme>
 #include <KLocalizedContext>
+#include <KLocalizedQmlContext>
 #include <KLocalizedString>
+#include <KirigamiAddons/App/KirigamiApp>
+#include <KirigamiAddons/App/KirigamiAppDefaults>
 #include <QApplication>
 #include <QIcon>
 #include <QQmlApplicationEngine>
@@ -23,25 +27,29 @@
 
 int main(int argc, char *argv[])
 {
-    KIconTheme::initTheme();
     QApplication app(argc, argv);
-    KLocalizedString::setApplicationDomain("karton");
-    QApplication::setOrganizationName(QStringLiteral("KDE"));
-    QApplication::setOrganizationDomain(QStringLiteral("kde.org"));
-    QApplication::setApplicationName(QStringLiteral("Karton"));
-    QApplication::setDesktopFileName(QStringLiteral("org.kde.karton"));
-    QGuiApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("org.kde.karton")));
+    KirigamiAppDefaults::apply(&app);
 
-    if (qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE")) {
-        QQuickStyle::setStyle(QStringLiteral("org.kde.desktop"));
-    }
+    KLocalizedString::setApplicationDomain("karton");
+    KAboutData about(QStringLiteral("karton"),
+                     i18nc("Application name", "Karton"),
+                     QStringLiteral("1.0"),
+                     i18nc("Application description", "Manage and run virtual machines with ease"),
+                     KAboutLicense::GPL,
+                     i18nc("Copyright statement", "(C) KDE"));
+
+    about.addAuthor(i18nc("author name", "Aaron Rainbolt"), i18nc("author role", "Initial developer"), QStringLiteral("arraybolt3@gmail.com"));
+    about.addAuthor(i18nc("author name", "Derek Lin"), i18nc("author role", "Developer"), QStringLiteral("derekhongdalin@gmail.com"));
+    about.setTranslator(i18nc("NAME OF TRANSLATORS", "Your names"), i18nc("EMAIL OF TRANSLATORS", "Your emails"));
+
+    KAboutData::setApplicationData(about);
+    QGuiApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("org.kde.karton")));
 
     QQmlApplicationEngine engine;
 
     qCInfo(KARTON_DEBUG) << "Hello! Starting Karton...";
 
-    engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
-
+    KLocalization::setupLocalizedContext(&engine);
     engine.loadFromModule("org.kde.karton", "Main");
 
     if (engine.rootObjects().isEmpty()) {
